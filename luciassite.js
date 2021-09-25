f4a30ee8872d96b63add8bdbb42ca1c4a470608a
casmanromawi <91203805+casmanromawi@users.noreply.github.com>
2021-09-25 08:21:24 +0700

    Update luciassite.js

--- luciassite.js
+++ luciassite.js
@@ -23,7 +23,7 @@ if(x=="2"){xxx="oppo.html"}
 if(x=="3"){xxx="car.html"}
 if(x=="4"){xxx="care.html"}
 if(x=="5"){xxx="bussines.html"}
-if(x=="6"){xxx="management.html"}
+if(x=="6"){xxx="managements.html"}
 if(x=="7"){xxx="health.html"}a_to_ve[i].href="http://www.luciascatering.com/2021/09/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
