ef3ea6f2bc2c62524a492199d24ac8c9d795bf00
casmanromawi <91203805+casmanromawi@users.noreply.github.com>
2021-09-25 08:12:57 +0700

    Update luciassite.js

--- luciassite.js
+++ luciassite.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*7)+1);var xxx=null;if(x=="1"){xxx="idekreatif.html"}
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*7)+1);var xxx=null;if(x=="1"){xxx="creatif.html"}
 if(x=="2"){xxx="opportunities.html"}
 if(x=="3"){xxx="careers.html"}
 if(x=="4"){xxx="healthcare.html"}
